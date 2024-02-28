9c81d5f485c15180e9663dd233a3f7ac08c67630
Patrice Torguet <patrice.torguet@irit.fr>
2024-02-28 18:25:01 +0100

    Tentative de filtrage des logs.

--- src/main/java/fr/miage/amc_proxy/AmcProxyApplication.java
+++ src/main/java/fr/miage/amc_proxy/AmcProxyApplication.java
@@ -1,10 +1,15 @@
 package fr.miage.amc_proxy;
 
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -21,6 +26,21 @@ public class AmcProxyApplication {
         SpringApplication.run(AmcProxyApplication.class, args);
     }
 
+
+    @Bean
+    @ConditionalOnMissingBean
+    ObservationRegistry observationRegistry() {
+        PathMatcher pathMatcher = new AntPathMatcher("/");
+        ObservationRegistry observationRegistry = ObservationRegistry.create();
+        observationRegistry.observationConfig().observationPredicate((name, context) -> {
+            if(context instanceof ServerRequestObservationContext) {
+                return !pathMatcher.match("/actuator/**", ((ServerRequestObservationContext) context).getCarrier().getRequestURI());
+            } else {
+                return true;
+            }
+        });
+        return observationRegistry;
+    }
 
 
 }
