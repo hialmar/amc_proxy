30d79193844609e8f5f2fe9b6dbfe2e2e3919183
Patrice Torguet <patrice.torguet@irit.fr>
2024-03-12 17:53:27 +0100

    Ajout Sécurité.

--- src/main/java/fr/miage/amc_proxy/AmcProxyApplication.java
+++ src/main/java/fr/miage/amc_proxy/AmcProxyApplication.java
@@ -1,18 +1,29 @@
 package fr.miage.amc_proxy;
 
-import io.micrometer.observation.ObservationRegistry;
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.server.observation.ServerRequestObservationContext;
-import org.springframework.util.AntPathMatcher;
-import org.springframework.util.PathMatcher;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableWebFluxSecurity
 public class AmcProxyApplication {
 
     @Bean
@@ -25,6 +36,38 @@ public class AmcProxyApplication {
     public static void main(String[] args) {
         SpringApplication.run(AmcProxyApplication.class, args);
     }
+
+
+    /*
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((requests) -> requests
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+
+        // process CORS annotations
+        //http.cors();
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // force a non-empty response body for 401's to make the response more browser friendly
+        Okta.configureResourceServer401ResponseBody(http);
+
+        return http.build();
+    }*/
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
 
 
 /*    @Bean
